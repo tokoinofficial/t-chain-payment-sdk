c22a65179822a525f7205ad910dc2be0e9dc8d30
Binh Nguyen <>
2022-09-08 09:57:11 +0700

    TW-1049 [Mobile] Push notification feature Add firebase message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:example/data/product.dart';
+import 'package:example/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:t_chain_payment_sdk/t_chain_payment_sdk.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
