8a3d3c952259aff10a0a07eb6334e1fc04c09698
Trần Thanh Vũ <82794492+vutrantokoin@users.noreply.github.com>
2023-04-14 15:21:04 +0700

    TW-1340 Add package references

--- example/lib/cubit/payment/payment_cubit.dart
+++ example/lib/cubit/payment/payment_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:example/data/fcm_notification.dart';
 import 'package:example/utils/constants.dart';
