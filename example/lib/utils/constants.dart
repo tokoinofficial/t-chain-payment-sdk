190152682d87f977eb106e9b14624af1f2b778bd
Trần Thanh Vũ <82794492+vutrantokoin@users.noreply.github.com>
2023-04-14 10:29:29 +0700

    TW-1331 Update key

--- example/lib/utils/constants.dart
+++ example/lib/utils/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
   static const String apiKey = '3e093592-3e0e-4a52-9601-ead49f794586';
   static const String bundleId = 'com.tokoin.tchainpayment.example';
-  // Enter your wallet's private key for testing
-  static const String privateKeyHex = '';
 }
